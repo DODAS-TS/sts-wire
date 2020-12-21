821584c54db04b1356fee82b1fcdbf744cc7140b
MircoT <MircoT@users.noreply.github.com>
2020-12-21 10:17:25 +0100

    improve path validator

--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -5,31 +5,35 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
 )
 
-var ErrNoValidPath = errors.New("no valid path")
+var (
+	ErrNoValidPath = errors.New("no valid path")
+	validPath      = regexp.MustCompile(`^([a-z_\-\s0-9\.\/]+)+$`)
+)
 
 func LocalPath(localMountPoint string) (bool, error) {
+	if !validPath.MatchString(localMountPoint) {
+		return false, ErrNoValidPath
+	}
+
 	absPath, errAbs := filepath.Abs(filepath.Clean(localMountPoint))
 	if errAbs != nil {
 		return false, fmt.Errorf("no valid path: %w", errAbs)
 	}
 
-	targetFolder, errStat := os.Stat(absPath)
+	_, errStat := os.Stat(absPath)
 	if errStat != nil && !os.IsNotExist(errStat) {
 		return false, fmt.Errorf("no valid path: %w", errStat)
 	}
 
-	if os.IsNotExist(errStat) {
-		errMkdir := os.Mkdir(absPath, os.ModePerm)
-		if errMkdir != nil {
-			return false, fmt.Errorf("no valid path: %w", errStat)
-		}
-
-		os.RemoveAll(absPath)
-	} else if !targetFolder.IsDir() {
-		return false, ErrNoValidPath
+	errMkdir := os.MkdirAll(absPath, os.ModePerm)
+	if errMkdir != nil {
+		return false, fmt.Errorf("no valid path: %w", errStat)
 	}
+
+	os.RemoveAll(absPath)
 
 	parentFolder, errStat := os.Stat(filepath.Dir(absPath))
 	if errStat != nil {
