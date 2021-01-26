1561da005a95320bf8a2ad51fcfa7676de80762f
MircoT <MircoT@users.noreply.github.com>
2021-01-26 11:58:09 +0100

    Fix container machine id in encryption

--- pkg/core/encryption.go
+++ pkg/core/encryption.go
@@ -1,13 +1,16 @@
 package core
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/hmac"
 	"crypto/md5"
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
 	"io"
+	"os"
 	"strings"
 
 	"github.com/awnumar/memguard"
@@ -15,14 +18,56 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+func tryContainerMachineID() (machineID string, err error) {
+	// Ref: https://stackoverflow.com/questions/23513045/how-to-check-if-a-process-is-running-inside-docker-container
+	cgroupFile, err := os.Open("/proc/self/cgroup")
+	if err != nil {
+		return machineID, fmt.Errorf("cannot open cgroup: %w", err)
+	}
+
+	var buff bytes.Buffer
+
+	_, err = buff.ReadFrom(cgroupFile)
+	if err != nil {
+		return machineID, fmt.Errorf("cannot read cgroup: %w", err)
+	}
+
+	for _, line := range strings.Split(buff.String(), "\n") {
+		if strings.Contains(line, "/docker/") {
+			parts := strings.Split(line, "/docker/")
+			if len(parts) != 2 {
+				return machineID, fmt.Errorf("not a valid docker container id: %w", err)
+			}
+
+			machineID = parts[1]
+
+			break
+		}
+	}
+
+	if machineID == "" {
+		return machineID, fmt.Errorf("docker container id not found: %w", err)
+	}
+
+	return machineID, nil
+}
+
 func CreateHash(key string) string {
 	log.Debug().Msg("create hash")
 
 	id, errID := machineid.ProtectedID("sts-wire")
 	if errID != nil {
 		if strings.Contains(errID.Error(), "open /etc/machine-id: no such file or directory") {
-			id = "notAMachine"
-			log.Debug().Str("machineID", id).Msg("Cannot create an hash for the machine")
+			// TODO: get a unique uuid for containers:
+			// Ref: https://github.com/denisbrodbeck/machineid/issues/5
+			// Ref: https://github.com/panta/machineid/blob/master/id_linux.go
+			id, errID = tryContainerMachineID()
+			if errID != nil {
+				id = "notAMachine"
+				log.Debug().Str("machineID", id).Msg("Cannot find a docker container id")
+			} else {
+				log.Debug().Str("machineID", id).Msg("Found docker container id")
+			}
 		} else {
 			panic(errID)
 		}
