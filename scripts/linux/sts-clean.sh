b694879c75ba1151c6a6127cc1ee83596c0c9d82
Federico Fornari <federico.fornari@cnaf.infn.it>
2023-10-26 17:50:58 +0200

    Optimized sts-clean.sh

--- scripts/linux/sts-clean.sh
+++ scripts/linux/sts-clean.sh
@@ -2,4 +2,5 @@
 for pid in $(pgrep sts-mount.sh); do kill -9 $pid; done
 for pid in $(pgrep oidc-agent); do kill -9 $pid; done
 for pid in $(pgrep sleep); do kill -9 $pid; done
-rm -rf .myRGW/ /tmp/token
+sts-wire clean
+rm -f /tmp/token
