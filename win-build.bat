7142caa48c4b64300444d455af7660a78af92b8d
Federico Fornari <federico.fornari@cnaf.infn.it>
2023-10-29 07:43:54 +0100

    Updated win-build.bat

--- win-build.bat
+++ win-build.bat
@@ -1,5 +1,5 @@
 @echo off
 
-set "psCommand=powershell -Command ""$pattern = '(?s)(UNAME_S = \$\(shell uname -s\).*?\r?\nendif)'; $content = Get-Content -Raw -Path 'Makefile'; $editedText = $content -replace $pattern, ''; Set-Content -Value $editedText -Path 'Makefile'; mingw32-make.exe build-windows-with-rclone;""
+set "psCommand=powershell -Command ""$pattern = '(?s)(UNAME_S = \$\(shell uname -s\).*?\r?\nendif)'; $content = Get-Content -Raw -Path 'Makefile'; $editedText = $content -replace $pattern, ''; Set-Content -Value $editedText -Path 'Makefile'; $pattern = 'BUILTTIME =.*'; $content = Get-Content -Raw -Path 'Makefile'; $editedText = $content -replace $pattern, 'BUILTTIME := $(shell powershell -Command "Get-Date -Format MM-dd-yy")'; Set-Content -Value $editedText -Path 'Makefile'; mingw32-make.exe build-windows-with-rclone;""
 
 %psCommand%
