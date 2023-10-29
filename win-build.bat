76f4356d3173f6833a83935796493c946a900634
Federico Fornari <federico.fornari@cnaf.infn.it>
2023-10-29 10:16:52 +0100

    Setup env vars in Windows powershell for build

--- win-build.bat
+++ win-build.bat
@@ -1,5 +1,5 @@
 @echo off
 
-set "psCommand=powershell -Command ""$pattern = '(?s)(UNAME_S = \$\(shell uname -s\).*?\r?\nendif)'; $content = Get-Content -Raw -Path 'Makefile'; $editedText = $content -replace $pattern, ''; Set-Content -Value $editedText -Path 'Makefile'; $pattern = 'BUILTTIME =.*'; $content = Get-Content -Raw -Path 'Makefile'; $editedText = $content -replace $pattern, 'BUILTTIME := $(shell powershell -Command "Get-Date -Format MM-dd-yy")'; Set-Content -Value $editedText -Path 'Makefile'; mingw32-make.exe build-windows-with-rclone;""
+set "psCommand=powershell -Command ""$env:CPATH='C:\Program Files (x86)\WinFsp\inc\fuse'; $env:INCLUDE='C:\Program Files\mingw64\include'; $env:PATH='C:\Program Files\mingw64\bin;C:\Program Files\mingw64\lib;' + $env:PATH; $pattern = '(?s)(UNAME_S = \$\(shell uname -s\).*?\r?\nendif)'; $content = Get-Content -Raw -Path 'Makefile'; $editedText = $content -replace $pattern, ''; Set-Content -Value $editedText -Path 'Makefile'; $pattern = 'BUILTTIME =.*'; $content = Get-Content -Raw -Path 'Makefile'; $editedText = $content -replace $pattern, 'BUILTTIME := $(shell powershell -Command "Get-Date -Format MM-dd-yy")'; Set-Content -Value $editedText -Path 'Makefile'; mingw32-make.exe build-windows-with-rclone;""
 
 %psCommand%
